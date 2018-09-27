2a6434064162e246172b126c25df9813f954e4fe
Michał Worwąg <michalwo@gmail.com>
2018-09-27 13:18:12 +0200

    Finished

---
+++ README.md
@@ -0,0 +1,5 @@
+JS Clock with CSS
+
+https://michal-worwag.github.io/js-clock-with-css/
+
+Excercise from JavaScript30
---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const secondHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+function setDate(){
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minutesHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
